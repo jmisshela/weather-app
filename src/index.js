db81ce39841427f22c86b14aa04198d4777abf45
jmisshela <138009686+jmisshela@users.noreply.github.com>
2024-02-05 10:42:17 +0700

    Injected the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -55,7 +55,29 @@ function handleSearch(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">❄️</div>
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">15°</span>
+            <span class="weather-forecast-temperature-min">9°</span>
+          </div>
+        </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
 
 searchCity("Oslo");
+displayForecast();
